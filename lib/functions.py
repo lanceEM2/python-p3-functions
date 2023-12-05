503a642ed56600f88c9d763f3f69f4de6161d5ce
Dylan <dylanlancekemboi@gmail.com>
2023-12-05 12:20:48 +0300

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,24 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
+greet_with_default("programmer")
 
 def add(num1, num2):
     pass
+    return(num1 + num2)
+
+add(45, 55)
 
 def halve(number):
     pass
+    return(number / 2)
